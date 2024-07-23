dce356cdd01fbdf1c8127f479d9672d27e5f502c
JJHyeok13 <jjinh99@naver.com>
2024-07-23 20:49:29 +0900

    feat: 비밀번호 암호화 적용

--- domains/user/user.service.js
+++ domains/user/user.service.js
@@ -1,13 +1,20 @@
+import crypto from "crypto";
 import { findUser, insertUser } from "./user.dao.js";
 
 export const signupUser = async (userInfo) => {
   const { name, nickname, email, password } = userInfo;
 
+  // 비밀번호 해싱
+  const hashedPassword = crypto
+    .createHash("sha256")
+    .update(password)
+    .digest("hex");
+
   const signupUserData = await insertUser({
     name: name,
     nickname: nickname,
     email: email,
-    password: password,
+    password: hashedPassword,
   });
 
   const userData = await findUser(signupUserData);
