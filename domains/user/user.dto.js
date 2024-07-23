38077ec19b2c33cbde5b70aad3919e144ae627de
JJHyeok13 <jjinh99@naver.com>
2024-07-23 18:03:10 +0900

    feat: UserDTO 정의

---
+++ domains/user/user.dto.js
@@ -0,0 +1,10 @@
+class UserDTO {
+  constructor(name, nickname, email, password) {
+    this.name = name;
+    this.nickname = nickname;
+    this.email = email;
+    this.password = password;
+  }
+}
+
+export default UserDTO;
