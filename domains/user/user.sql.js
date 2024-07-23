ad13f7a4583779e6e5ca3d3983bb2d29a6c677bd
JJHyeok13 <jjinh99@naver.com>
2024-07-23 22:59:12 +0900

    feat: 이메일로 유저 찾기 SQL문 작성

--- domains/user/user.sql.js
+++ domains/user/user.sql.js
@@ -3,7 +3,12 @@ export const userSignUpSQL = `
     INSERT INTO User (name, nickname, email, password) VALUES (?, ?, ?, ?)
 `;
 
-// ID 값으로 Member 찾기
+// ID 값으로 User 찾기
 export const getUserById = `
   SELECT * FROM User WHERE id = ?
+`;
+
+// 이메일로 User 찾기
+export const getUserByEmail = `
+  SELECT * FROM User WHERE email = ?
 `;
