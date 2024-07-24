252007e7f74187bef91121c47c3825e0ee693d8c
gs0428 <lider28@naver.com>
2024-07-25 01:11:29 +0900

    feat: 클라이언트에서 요청한 이메일 주소로 인증 코드 보내는 로직 구현

--- domains/user/user.controller.js
+++ domains/user/user.controller.js
@@ -1,5 +1,6 @@
 import { response } from "../../config/response.js";
 import { status } from "../../config/response.status.js";
+import { sendCodeEmail } from "../../utils/email.js";
 
 import { loginUser, signupUser } from "./user.service.js";
 
@@ -23,6 +24,19 @@ export const userLogin = async (req, res, next) => {
     const { email, password } = req.body;
     const result = await loginUser(email, password);
     res.status(200).json(response(status.SUCCESS, result));
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const userCreateCode = async (req, res, next) => {
+  try {
+    console.log("이메일 코드 생성 요청");
+    console.log("body: ", req.body);
+
+    const { email } = req.body;
+    const { to, code } = sendCodeEmail(email);
+    res.status(200).json(response(status.SUCCESS, "이메일 코드 생성 완료"));
   } catch (error) {
     next(error);
   }
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,10 +1,12 @@
 import express from "express";
 import expressAsyncHandler from "express-async-handler";
 
-import { userLogin, userSignUp } from "../domains/user/user.controller.js";
+import { userCreateCode, userLogin, userSignUp } from "../domains/user/user.controller.js";
 
 export const userRouter = express.Router();
 
 userRouter.post("/signup", expressAsyncHandler(userSignUp));
 
 userRouter.post("/login", expressAsyncHandler(userLogin));
+
+userRouter.post("/create-code", expressAsyncHandler(userCreateCode));
