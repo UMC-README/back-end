3c1523b38d04ae21a813d63589bc809ef0031a5e
JJHyeok13 <jjinh99@naver.com>
2024-07-24 21:35:50 +0900

    rename: 테이블 소문자로 변경에 따른 sql문 수정 (User -> user)

--- domains/user/user.sql.js
+++ domains/user/user.sql.js
@@ -1,14 +1,14 @@
 // 회원가입
 export const userSignUpSQL = `
-    INSERT INTO User (name, nickname, email, password) VALUES (?, ?, ?, ?)
+    INSERT INTO user (name, nickname, email, password) VALUES (?, ?, ?, ?)
 `;
 
 // ID 값으로 User 찾기
 export const getUserById = `
-  SELECT * FROM User WHERE id = ?
+  SELECT * FROM user WHERE id = ?
 `;
 
 // 이메일로 User 찾기
 export const getUserByEmail = `
-  SELECT * FROM User WHERE email = ?
+  SELECT * FROM user WHERE email = ?
 `;
