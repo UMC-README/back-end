a23a9af1ad4eefd989a748dd0afde881728c4d17
parkmineum <a01087164428@gmail.com>
2024-07-26 03:44:46 +0900

    fix : dto 삭제 및 요청 사항 반영

--- domains/admin/admin.dao.js
+++ domains/admin/admin.dao.js
@@ -7,9 +7,7 @@ export const getUserProfile = async (userId) => {
   try {
     const conn = await pool.getConnection();
 
-    // console.log("query 조회 전");
     const [rows] = await conn.query(getProfileByUserId, userId);
-    // console.log("query 조회 후");
     if (rows.length === 0) {
       conn.release();
       throw new BaseError(404, "User not found");
--- domains/admin/admin.dto.js
+++
@@ -1,7 +0,0 @@
-export class UserProfileDto {
-  constructor(nickname, profileImage, penaltyCount) {
-    this.nickname = nickname;
-    this.profileImage = profileImage;
-    this.penaltyCount = penaltyCount;
-  }
-}
