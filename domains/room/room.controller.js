95a883a735c8b3c1bbd99e7779b9d7d6d993efc2
jshong99 <widejs@gmail.com>
2024-07-25 19:38:36 +0900

    feat: 고정 공지글 삭제 API 구현

--- domains/room/room.controller.js
+++ domains/room/room.controller.js
@@ -1,14 +1,31 @@
 import { response } from "../../config/response.js";
 import { status } from "../../config/response.status.js";
 
-import { postFix } from "./room.service.js";
+import { deletePostFix, postFix } from "./room.service.js";
 
 export const fixPost = async (req, res, next) => {
   try {
-    console.log("고정 공지글 변경");
-    console.log("body: ", req.body);
+    console.log("고정 공지글 등록");
 
-    const result = await postFix(req.body);
+    const userId = req.user.userId;
+    console.log("body: ", req.body);
+    console.log("userId: ", userId);
+
+    const result = await postFix(req.body, userId);
+    res.status(200).json(response(status.SUCCESS, result));
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteFixPost = async (req, res, next) => {
+  try {
+    console.log("고정 공지글 삭제");
+
+    const userId = req.user.userId;
+    console.log("userId: ", userId);
+
+    const result = await deletePostFix(userId);
     res.status(200).json(response(status.SUCCESS, result));
   } catch (error) {
     next(error);
