a622e245e9f304f90bd51ad0c2c8cec6c54be016
jshong99 <widejs@gmail.com>
2024-07-25 19:38:36 +0900

    feat: 고정 공지글 삭제 API 구현

--- routes/room.route.js
+++ routes/room.route.js
@@ -1,8 +1,11 @@
 import express from "express";
 import expressAsyncHandler from "express-async-handler";
 
-import { fixPost } from "../domains/room/room.controller.js";
+import { fixPost, deleteFixPost } from "../domains/room/room.controller.js";
+
+import { tokenAuth } from "../middleware/token.auth.js";
 
 export const roomRouter = express.Router();
 
-roomRouter.patch("/fixPost", expressAsyncHandler(fixPost));
+roomRouter.patch("/fixPost", tokenAuth, expressAsyncHandler(fixPost));
+roomRouter.delete("/fixPost", tokenAuth, expressAsyncHandler(deleteFixPost));
