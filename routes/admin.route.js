fffa3d2ebee493be0a97dd9cd6119d2e0f6b7e7f
parkmineum <a01087164428@gmail.com>
2024-07-25 21:43:22 +0900

    fix :  라우트 tokenAuth 추가

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,8 +1,8 @@
 import express from "express";
 import expressAsyncHandler from "express-async-handler";
-
-import { userProfile } from "../domains/room_admin/admin.controller.js";
+import { tokenAuth } from "../middleware/token.auth.js";
+import { userProfile } from "../domains/admin/admin.controller.js";
 
 export const adminRouter = express.Router();
 
-adminRouter.get("/profile/:user_id", expressAsyncHandler(userProfile));
+adminRouter.get("/profile/:user_id", tokenAuth, expressAsyncHandler(userProfile));
