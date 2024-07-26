c661c214ab27ce1bf09dbf65539dd664e5d28cee
jshong99 <widejs@gmail.com>
2024-07-26 21:25:54 +0900

    feat: 전체 공지글 조회 router, controller 작성

--- domains/room/room.controller.js
+++ domains/room/room.controller.js
@@ -1,7 +1,7 @@
 import { response } from "../../config/response.js";
 import { status } from "../../config/response.status.js";
 
-import { deletePostFix, postFix } from "./room.service.js";
+import { deletePostFix, postFix, getAllPostSER } from "./room.service.js";
 
 export const fixPost = async (req, res, next) => {
   try {
@@ -27,6 +27,23 @@ export const deleteFixPost = async (req, res, next) => {
     console.log("userId: ", userId);
 
     const result = await deletePostFix(userId);
+    res.status(200).json(response(status.SUCCESS, result));
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getAllPost = async (req, res, next) => {
+  try {
+    console.log("공지방 내 모든 공지글 조회");
+
+    const userId = req.user.userId;
+    const roomId = req.params;
+
+    console.log("userId: ", userId);
+    console.log("roomId: ", userId);
+
+    const result = await getAllPostSER(userId, roomId);
     res.status(200).json(response(status.SUCCESS, result));
   } catch (error) {
     next(error);
