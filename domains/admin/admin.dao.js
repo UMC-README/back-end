a577437f46eee39a1e0a819e7c9b08c099fff922
parkmineum <a01087164428@gmail.com>
2024-07-26 16:40:34 +0900

    feat : 공지방 생성 route,sql,dao 구현

--- domains/admin/admin.dao.js
+++ domains/admin/admin.dao.js
@@ -1,7 +1,7 @@
 import { pool } from "../../config/db.config.js";
 import { BaseError } from "../../config/error.js";
 import { status } from "../../config/response.status.js";
-import { getProfileByUserId } from "./admin.sql.js";
+import { createRoomsByUser, getProfileByUserId } from "./admin.sql.js";
 
 export const getUserProfile = async (userId) => {
   try {
@@ -17,6 +17,23 @@ export const getUserProfile = async (userId) => {
     return rows[0];
   } catch (error) {
     console.log("User 프로필 조회 에러");
+    throw new BaseError(status.INTERNAL_SERVER_ERROR);
+  }
+};
+
+export const createRoomsDao = async (userId) => {
+  try {
+    const conn = await pool.getConnection();
+    const [rows] = await conn.query(createRoomsByUser, [userId]);
+
+    if (rows.length === 0) {
+      throw new BaseError(404, "User not found");
+    }
+
+    conn.release();
+    return rows[0];
+  } catch (error) {
+    console.error("공지방 생성하기 에러");
     throw new BaseError(status.INTERNAL_SERVER_ERROR);
   }
 };
