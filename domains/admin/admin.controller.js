6709c3972be7f9d04bcf3f2897c9e9e4485b3bf0
parkmineum <a01087164428@gmail.com>
2024-07-27 01:51:08 +0900

    feat : API 체크

--- domains/admin/admin.controller.js
+++ domains/admin/admin.controller.js
@@ -6,9 +6,53 @@ import { createRoomsService, getProfileUser } from "./admin.service.js";
 
 export const createRoomsController = async (req, res, next) => {
   try {
-    console.log("공지방 생성하기");
-    const userId = req.user.user_id;
-    const result = await createRoomsService(userId);
+    // 요청 바디에서 데이터 추출
+    const {
+      admin_id,
+      admin_nickname,
+      room_name,
+      room_password,
+      room_image,
+      room_invite_url,
+      max_penalty,
+    } = req.body;
+
+    // 필수 필드 확인
+    if (
+      !admin_id ||
+      !admin_nickname ||
+      !room_name ||
+      !room_password ||
+      !room_image ||
+      !room_invite_url ||
+      !max_penalty
+    ) {
+      return res.status(400).json({ error: "모든 필수 필드를 입력하세요." });
+    }
+
+    // 데이터베이스 쿼리 실행
+    const result = await createRoomsService({
+      admin_id,
+      admin_nickname,
+      room_name,
+      room_password,
+      room_image,
+      room_invite_url,
+      max_penalty,
+    });
+
+    // 응답 객체 구성
+    const response = {
+      // id: result.insertId,
+      admin_id,
+      admin_nickname,
+      room_name,
+      room_password,
+      room_image,
+      room_invite_url,
+      max_penalty,
+    };
+
     res.status(200).json(response(status.SUCCESS, result));
   } catch (error) {
     next(error);
--- domains/admin/admin.sql.js
+++ domains/admin/admin.sql.js
@@ -1,14 +1,12 @@
 // 공지방 생성
-export const createRoomsByUserId = ` 
-  SELECT r.admin_nickname, r.room_name, r.room_password, r.max_penalty
-  FROM room r
-  JOIN user u ON u.id = r.user_id
-  WHERE u.id = ? AND r.status = 'EXIST'   
+export const createRoomsSQL = `
+  INSERT INTO room (admin_id, admin_nickname, room_name, room_password, room_image, max_penalty, room_invite_url)
+  VALUES (?, ?, ?, ?, ?, ?, ?) 
 `;
 
 // 유저 프로필 조회
 export const getProfileByUserId = `
-  SELECT ur.nickname, ur.profile_image, ur.penalty_count
+  SELECT ur.nickname    , ur.profile_image, ur.penalty_count
   FROM \`user-room\` ur
   JOIN user u ON u.id = ur.user_id
   WHERE u.id = ?
