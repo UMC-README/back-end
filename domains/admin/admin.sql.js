84a6be27ae127a2e05f16ae7ca465746193c36c9
parkmineum <a01087164428@gmail.com>
2024-07-26 16:55:33 +0900

    Wls

--- domains/admin/admin.sql.js
+++ domains/admin/admin.sql.js
@@ -1,15 +1,15 @@
-// 유저 프로필 조회
-export const getProfileByUserId = `
-  SELECT ur.nickname, ur.profile_image, ur.penalty_count
-  FROM \`user-room\` ur
-  JOIN user u ON u.id = ur.user_id
-  WHERE u.id = ?
-`;
-
 // 공지방 생성
 export const createRoomsByUser = ` 
   SELECT r.admin_nickname, r.room_name, r.room_password, r.max_penalty
   FROM room r
   JOIN user u ON u.id = r.user_id
   WHERE u.id = ? AND r.status = 'EXIST'   
+`;
+
+// 유저 프로필 조회
+export const getProfileByUserId = `
+  SELECT ur.nickname, ur.profile_image, ur.penalty_count
+  FROM \`user-room\` ur
+  JOIN user u ON u.id = ur.user_id
+  WHERE u.id = ?
 `;
