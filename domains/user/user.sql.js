1c06b0f3fadc92c90653c7a324073e00901a9235
JJHyeok13 <jjinh99@naver.com>
2024-07-27 22:54:57 +0900

    feat: 입장한, 개설한 공지방 개수 구하기 SQL문 작성

--- domains/user/user.sql.js
+++ domains/user/user.sql.js
@@ -21,6 +21,14 @@ export const getFixedPost = `
   WHERE u.id = ?
 `;
 
+// 개설한 공지방 개수 구하기
+export const getCreateRoomCount = `
+  SELECT COUNT(*) as count
+  FROM user u
+  JOIN room r ON u.id = r.admin_id
+  WHERE u.id = ?
+`;
+
 // 개설한 공지방 찾기
 export const getCreateRoom = `
   SELECT r.*
@@ -28,6 +36,15 @@ export const getCreateRoom = `
   JOIN room r ON u.id = r.admin_id
   WHERE u.id = ?
   LIMIT ? OFFSET ?
+`;
+
+// 입장한 공지방 개수 구하기
+export const getJoinRoomCount = `
+  SELECT COUNT(*) as count
+  FROM \`user-room\` ur
+  JOIN user u ON u.id = ur.user_id
+  JOIN room r ON r.id = ur.room_id
+  WHERE u.id = ?
 `;
 
 // 입장한 공지방 찾기
