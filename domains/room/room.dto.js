e7279a50a477fb70e809109f209f5e32606526ae
jshong99 <widejs@gmail.com>
2024-07-29 17:53:23 +0900

    feat: 공지방 내 완료하지 않은 공지글 조회 DTO 작성

--- domains/room/room.dto.js
+++ domains/room/room.dto.js
@@ -19,7 +19,7 @@ export const allPostInRoomDTO = (data) => {
 
 const formatSubmitState = (data) => {
   if (data == null) {
-    return "notComplete";
+    return "NOT_COMPLETE";
   } else return data;
 };
 
@@ -30,4 +30,39 @@ const formatDate = (date) => {
     hour12: false,
   };
   return new Intl.DateTimeFormat("ko", options).format(new Date(date));
+};
+
+export const notCheckedPostInRoomDTO = (data) => {
+  const posts = [];
+  const length = 3;
+
+  for (let i = 0; i < length; i++) {
+    posts.push({
+      roomName: data[i].room_name,
+      postId: data[i].id,
+      postTitle: data[i].title,
+      updatedAtBefore: elapsedTime(data[i].updatedAtBeforeSec),
+    });
+  }
+  return { postData: posts };
+};
+
+const elapsedTime = (data) => {
+  const seconds = data;
+  if (seconds < 60) return "방금 전";
+
+  const minutes = seconds / 60;
+  if (minutes < 60) return `${Math.floor(minutes)}분 전`;
+
+  const hours = minutes / 60;
+  if (hours < 24) return `${Math.floor(hours)}시간 전`;
+
+  const days = hours / 24;
+  if (days < 7) return `${Math.floor(days)}일 전`;
+
+  const months = days / 30;
+  if (months < 12) return `${Math.floor(months)}개월 전`;
+
+  const years = months / 12;
+  return `${Math.floor(years)}년 전`;
 };
