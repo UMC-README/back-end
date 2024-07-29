d4aefda0f5138ad0c0adc9b494adc45b1ef5e93b
jshong99 <widejs@gmail.com>
2024-07-29 17:52:59 +0900

    feat: 공지방 내 완료하지 않은 공지글 조회 controller 작성

--- domains/room/room.controller.js
+++ domains/room/room.controller.js
@@ -1,7 +1,12 @@
 import { response } from "../../config/response.js";
 import { status } from "../../config/response.status.js";
 
-import { deletePostFix, postFix, getAllPostInRoom } from "./room.service.js";
+import {
+  deletePostFix,
+  postFix,
+  getAllPostInRoom,
+  getNotCheckedPostInRoom,
+} from "./room.service.js";
 
 export const fixPost = async (req, res, next) => {
   try {
@@ -44,6 +49,23 @@ export const getAllPost = async (req, res, next) => {
     console.log("userId: ", userId);
 
     const result = await getAllPostInRoom(roomId, userId, req.query);
+    res.status(200).json(response(status.SUCCESS, result));
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getNotCheckedPost = async (req, res, next) => {
+  try {
+    console.log("공지방 내 완료하지 않은 공지글 조회");
+
+    const roomId = req.params.roomId;
+    const userId = req.user.userId;
+
+    console.log("roomId: ", roomId);
+    console.log("userId: ", userId);
+
+    const result = await getNotCheckedPostInRoom(roomId, userId, req.query);
     res.status(200).json(response(status.SUCCESS, result));
   } catch (error) {
     next(error);
