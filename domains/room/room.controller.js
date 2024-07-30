37853b953214bf9c95e3d1898b5286596d5039a0
jshong99 <widejs@gmail.com>
2024-07-30 20:32:31 +0900

    feat: controller 작성

--- domains/room/room.controller.js
+++ domains/room/room.controller.js
@@ -6,6 +6,7 @@ import {
   postFix,
   getAllPostInRoom,
   getNotCheckedPostInRoom,
+  getDetailedPostSer,
 } from "./room.service.js";
 
 export const fixPost = async (req, res, next) => {
@@ -66,6 +67,23 @@ export const getNotCheckedPost = async (req, res, next) => {
     console.log("userId: ", userId);
 
     const result = await getNotCheckedPostInRoom(roomId, userId);
+    res.status(200).json(response(status.SUCCESS, result));
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getDetailedPost = async (req, res, next) => {
+  try {
+    console.log("개별 공지글 상세 조회");
+
+    const postId = req.params.postId;
+    const userId = req.user.userId;
+
+    console.log("postId: ", postId);
+    console.log("userId: ", userId);
+
+    const result = await getDetailedPostSer(postId, userId);
     res.status(200).json(response(status.SUCCESS, result));
   } catch (error) {
     next(error);
