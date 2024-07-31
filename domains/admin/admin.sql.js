7044afd8583f19defbd720e5963e1c367e7e61e3
parkmineum <a01087164428@gmail.com>
2024-07-31 22:37:45 +0900

    feat : 공지글 수정 sql 구현

--- domains/admin/admin.sql.js
+++ domains/admin/admin.sql.js
@@ -18,7 +18,7 @@ export const deleteRoomsSQL = `
     UPDATE room SET updated_at = NOW(), state = 'deleted' WHERE id = ?;
 `;
 
-// 공지글 생성 & 공지방 이미지
+// 공지글, 공지방 이미지 생성
 export const createPostSQL = `
   INSERT INTO post (room_id, title, content, type, start_date, end_date, question, unread_count, user_id)
   VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?);  
@@ -29,6 +29,17 @@ export const getMemberCountSQL = `
 `;
 export const createPostImgSQL = `
   INSERT INTO \`post-image\` (URL, post_id) VALUES(?,?);  
+`;
+
+// 공지글, 공지방 이미지 수정
+export const updatePostSQL = ` 
+  UPDATE post
+  SET title = ?, content = ?, start_date = ?, end_date = ?, question = ? 
+  WHERE id = ?;  
+`;
+
+export const deletePostImgSQL = `
+    UPDATE \`post-image\` SET state = 'DELETED' WHERE post_id = ? AND URL = ?;
 `;
 
 // 유저 프로필 조회
