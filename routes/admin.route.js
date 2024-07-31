3a8067c3ea55fa7c8c4db9a7d99ac0a2e21eac2f
parkmineum <a01087164428@gmail.com>
2024-07-31 22:37:02 +0900

    feat : 공지글 수정 route 구현

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -6,6 +6,7 @@ import {
   updateRoomsController,
   deleteRoomsController,
   createPostController,
+  updatePostController,
   userProfile,
 } from "../domains/admin/admin.controller.js";
 
@@ -15,4 +16,5 @@ adminRouter.post("/rooms", tokenAuth, expressAsyncHandler(createRoomsController)
 adminRouter.patch("/rooms/:room_id", tokenAuth, expressAsyncHandler(updateRoomsController));
 adminRouter.patch("/rooms", tokenAuth, expressAsyncHandler(deleteRoomsController));
 adminRouter.post("/post", tokenAuth, expressAsyncHandler(createPostController));
+adminRouter.patch("/post/:post_id", tokenAuth, expressAsyncHandler(updatePostController));
 adminRouter.get("/profile/:user_id", tokenAuth, expressAsyncHandler(userProfile));
