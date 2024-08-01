25ad9cae95bba65c08bc5014c242115fa35a0e9a
jshong99 <widejs@gmail.com>
2024-08-01 20:48:37 +0900

    chore: merge 중 중복 발생 삭제

--- domains/room/room.controller.js
+++ domains/room/room.controller.js
@@ -124,17 +124,3 @@ export const deleteComment = async (req, res, next) => {
     next(error);
   }
 };
-
-export const deleteComment = async (req, res, next) => {
-  try {
-    const commentId = req.params.commentId;
-    const userId = req.user.userId;
-    console.log("commentId: ", commentId);
-    console.log("userId: ", userId);
-
-    const result = await deleteCommentSer(commentId, userId);
-    res.status(200).json(response(status.SUCCESS, result));
-  } catch (error) {
-    next(error);
-  }
-};
