18e291afca529b1959c3e5d6e81a97df689d49a6
gs0428 <lider28@naver.com>
2024-08-14 16:12:33 +0900

    feat: date 관련 util 함수 구현

---
+++ utils/date.js
@@ -0,0 +1,14 @@
+export const getToday = () => {
+  const UTCtime = new Date(new Date().setHours(0, 0, 0, 0));
+  const today = new Date(new Date(UTCtime).setHours(UTCtime.getHours() + 9));
+  return today.getTime();
+};
+
+export const getDate = (date) => {
+  const convertDate = new Date(`20${date}`);
+  const hour = convertDate.getHours() + 9;
+  return new Date(new Date(convertDate).setHours(hour)).getTime();
+};
+
+export const isInvalidDate = (startDate, endDate) =>
+  isNaN(new Date(`20${startDate}`)) || isNaN(new Date(`20${endDate}`));
